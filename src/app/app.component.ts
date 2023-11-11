0880666a5093a0f164396fe5f733dae1a1726207
cavalown <cavalown@gmail.com>
2023-11-12 03:22:15 +0800

    feat: go to page target zone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'officialWebsiteOfLegislativeCandidate';
+  scrollToTarget(targetId:string): void {
+    const targetElement = document.getElementById(targetId);
+    targetElement?.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
 }
