c53a4d47d9029d7dcd5a6042148220dfaa9fcab1
cavalown <cavalown@gmail.com>
2023-11-18 20:56:33 +0800

    fix: footer animation reset animation & position

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,14 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  OnChanges,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 import { FooterService } from 'src/app/service/footer.service';
 
 @Component({
@@ -6,11 +16,33 @@ import { FooterService } from 'src/app/service/footer.service';
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent implements AfterViewInit{
+export class FooterComponent implements AfterViewInit, OnChanges {
   @ViewChild('footerImage', { static: false }) footerImage!: ElementRef;
-  constructor(public footerService: FooterService) {}
+  constructor(
+    public footerService: FooterService,
+    private router: Router,
+    private _cdr: ChangeDetectorRef
+  ) {}
 
-  ngAfterViewInit(){
-    this.footerService.play(this.footerImage);
+  ngAfterViewInit() {
+    this.router.events
+      .pipe(
+        filter(
+          (event): event is NavigationEnd => event instanceof NavigationEnd
+        )
+      )
+      .subscribe((event: NavigationEnd) => {
+        this.footerService.updateAnimationSettings(event.url);
+        setTimeout(() => {
+          this.footerService.play(this.footerImage);
+        }, 300);
+        this._cdr.detectChanges();
+      });
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['footerImage']) {
+      this.footerService.play(this.footerImage);
+    }
   }
 }
--- src/app/service/footer.service.ts
+++ src/app/service/footer.service.ts
@@ -63,77 +63,88 @@ export class FooterService {
     }
   }
 
+  public updateAnimationSettings(url: string) {
+    this.url = url;
+    this.setImageAnimationProps();
+  }
+
   public play(animationTarget: ElementRef) {
     const element = animationTarget.nativeElement;
-    setTimeout(() => {
-      switch (this.url) {
-        case '/home': {
-          gsap.to(element, {
-            x: 30,
-            y: 0,
-            duration: 0.5,
-            ease: 'power1.out',
-            repeat: -1,
-            yoyo: true,
-          });
-          break;
-        }
-        case '/activity': {
-          const tl = gsap.timeline({ repeat: -1, yoyo: true });
-          const elementWidth = element.clientWidth;
-          tl.to(element, {
-            x: () => `-${window.innerWidth - elementWidth}px`,
-            duration: 7,
+    gsap.killTweensOf(element);
+    gsap.set(element, { x: 0, y: 0, rotation: 0, rotationY: 0, scale: 1, opacity: 1 });
+    switch (this.url) {
+      case '/home': {
+        gsap.to(element, {
+          x: 30,
+          y: 0,
+          duration: 0.5,
+          ease: 'power1.out',
+          repeat: -1,
+          yoyo: true,
+        });
+        break;
+      }
+      case '/activity': {
+        const tl = gsap.timeline({ repeat: -1, yoyo: true });
+        const elementWidth = element.clientWidth;
+        tl.to(element, {
+          x: () => `-${window.innerWidth - elementWidth}px`,
+          duration: 7,
+          ease: 'power1.inOut',
+          yoyo: true,
+          repeat: -1,
+        });
+        tl.to(
+          element,
+          {
+            y: '10',
+            duration: 0.2,
             ease: 'power1.inOut',
             yoyo: true,
             repeat: -1,
-          });
-          tl.to(
-            element,
-            {
-              y: '10',
-              duration: 0.2,
-              ease: 'power1.inOut',
-              yoyo: true,
-              repeat: -1,
-            },
-            0
-          );
+          },
+          0
+        );
 
-          break;
-        }
-        case '/policy': {
-          const tl = gsap.timeline({ repeat: -1 });
-          const elementWidth = element.clientWidth;
-
-          tl.to(element, {
-            x: () => `-${window.innerWidth - elementWidth}px`,
-            y: -100,
-            duration: 8,
-            ease: 'power2.inOut',
-          })
-            .to(
-              element,
-              { rotationY: 180, rotation: 50, duration: 0.5 },
-              '-=0.25'
-            )
-            .to(
-              element,
-              { x: 0, y: 0, duration: 8, ease: 'power2.inOut' },
-              '+=0.5'
-            )
-            .to(element, { rotationY: 0, rotation: 0, duration: 0.5 }, '-=0.25')
-            .set(element, { rotationY: 0, rotation: 0 });
-          break;
-        }
-        case '/donate': {
-          gsap.to(element, { rotation: 360, duration: 3, ease: "linear", repeat: -1, yoyo:true });
-          break;
-        }
-        default: {
-          break;
-        }
+        break;
       }
-    });
+      case '/policy': {
+        const tl = gsap.timeline({ repeat: -1 });
+        const elementWidth = element.clientWidth;
+
+        tl.to(element, {
+          x: () => `-${window.innerWidth - elementWidth}px`,
+          y: -100,
+          duration: 8,
+          ease: 'power2.inOut',
+        })
+          .to(
+            element,
+            { rotationY: 180, rotation: 50, duration: 0.5 },
+            '-=0.25'
+          )
+          .to(
+            element,
+            { x: 0, y: 0, duration: 8, ease: 'power2.inOut' },
+            '+=0.5'
+          )
+          .to(element, { rotationY: 0, rotation: 0, duration: 0.5 }, '-=0.25')
+          .set(element, { rotationY: 0, rotation: 0 });
+        break;
+      }
+      case '/donate': {
+        gsap.to(element, {
+          rotation: 360,
+          duration: 3,
+          ease: 'linear',
+          repeat: -1,
+          yoyo: true,
+        });
+        break;
+      }
+      default: {
+        break;
+      }
+    }
   }
 }
